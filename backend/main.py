f7394fb2183817ee518c7a610abc4dcd11f8b894
ravnapieski <hansapieski@gmail.com>
2025-04-02 06:11:43 +0300

    🚨last tweakz then sending🚀this lvl10 gyat🍑to the interwebz🌐🥶

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 # main.py
+import os
+from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import rhyme_script
@@ -12,7 +14,7 @@ app.mount("/static", StaticFiles(directory="frontend/public"), name="static")
 # Allow requests from React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,16 +22,17 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup_event():
+    load_dotenv(find_dotenv())
+    wordlist_path = os.getenv("SANIT_FILE_PATH")
     # word list loaded once in app.state during startup, meaning it's
     # locked in, not floating around like a beta global var.
     # + built-in app lifecycle management
     try:
-        app.state.word_list = load_wordlist()  # Store word_list in state
+        app.state.word_list = load_wordlist(wordlist_path)  # Store word_list in state
     except Exception as e:
         return {"error": str(e)}
     
-def load_wordlist():
-    wordlist_path = "mat/sanit.txt"
+def load_wordlist(wordlist_path):
     try:
         with open(wordlist_path, "r") as file:
             word_list = file.readlines()
